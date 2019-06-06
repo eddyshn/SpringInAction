910dcf0eccb923ee3ec281b1dbfb3e65a76fd1e6
eddy <eddy.shan@ribitwo.com>
2019-06-06 16:16:03 +0800

    jpa

--- src/main/java/com/example/demo/Order.java
+++ src/main/java/com/example/demo/Order.java
@@ -3,22 +3,29 @@ package com.example.demo;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-public class Order {
+@Entity
+@Table(name="Taco_Order")
+public class Order implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
     private Date placedAt;
 
-//end::newFields[]
-
+    //end::allButDetailProperties[]
     @NotBlank(message="Delivery name is required")
     private String deliveryName;
 
@@ -44,18 +51,15 @@ public class Order {
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
 
+    @ManyToMany(targetEntity=Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addDesign(Taco design) {
         this.tacos.add(design);
     }
 
-  /*
-// tag::newFields[]
-  ...
-
-// end::newFields[]
-   */
-//tag::newFields[]
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
 }
-//end::newFields[]
--- src/main/java/com/example/demo/data/OrderRepository.java
+++ src/main/java/com/example/demo/data/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.data;
 
 import com.example.demo.Order;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
-    Order save(Order order);
+public interface OrderRepository extends CrudRepository<Order, Long> {
 }
